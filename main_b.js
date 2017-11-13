f273e6b0584813114029c7d2017bccf1012b4c21
miiamah <miia.mahonen@metropolia.fi>
2017-11-13 11:46:06 +0200

    Add files via upload

---
+++ main_b.js
@@ -0,0 +1,43 @@
+'use strict';
+
+// sivulla on p elementti 'message', jossa olisi tarkoitus näyttää palvelimen vastaus
+// valitse se, ja tallenna muuttujaan
+
+const message = document.querySelector('#message');
+
+
+// tee funktio 'upload', joka
+// - estää lomakkeen lähetyksen
+// - kirjoittaa 'message' elementtiin 'Upload in progress...'
+// - hakee lomakkeesta file kentän
+// - tekee FormData -olion ja lisää käyttäjän valitseman tiedoston siihen
+// - lähettää tiedoston fetch -metodilla osoitteeseen 'upload.php'
+// - kun tiedoston lähetys on valmis, kirjoittaa palvelimen vastauksen 'message' elementtiin
+
+const upload = (evt) => {
+  evt.preventDefault();
+
+  message.innerText = "Upload in progress...";
+
+  const input = document.querySelector('input[type="file"]');
+  const data = new FormData();
+
+  data.append('fileToUpload', input.files[0]);
+
+  const settings = {
+    method: 'POST',
+    body: data
+  };
+
+  fetch('upload.php', settings ).then((response) =>{
+    return response.text();
+  }).then((text) => {
+    console.log(text);
+    message.innerText = text;
+  });
+
+};
+
+// tee tapahtumakuuntelija, joka kutsuu 'upload' funktiota, kun lomake lähetetään
+
+document.querySelector('form').addEventListener("submit", upload);
